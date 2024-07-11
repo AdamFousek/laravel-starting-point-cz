2274b8fda2dcb81a20ea0a449c539ea2e0c900c6
adamfousek <adamfousekdev@gmail.com>
2024-07-11 21:39:24 +0200

    Remove public

--- public/build/assets/UpdateProfileInformationForm-B-QN7nzL.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./UpdateProfileInformationForm.vue_vue_type_script_setup_true_lang-C7SFn_lu.js";import"./app-DX7eUGuK.js";import"./TextInput.vue_vue_type_script_setup_true_lang-BeZCtYfw.js";import"./PrimaryButton-SQw6Bghm.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";export{o as default};
